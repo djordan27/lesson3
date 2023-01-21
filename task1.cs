898c858b8640bc3b746e7b458dbc73b4e27b3801
djordan27 <99352371+djordan27@users.noreply.github.com>
2023-01-21 20:02:44 +0300

    Update task1.cs

--- task1.cs
+++ task1.cs
@@ -1,36 +1,38 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-/*Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
-3 -> 1, 8, 27
-5 -> 1, 8, 27, 64, 125*/
+/*Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+14212 -> нет
+
+12821 -> да
+
+23432 -> да*/
 
 int GetMyNumber()
 {
-    int result = -1;
+    int result = 0;
 
     while (true)
     {
-        if (int.TryParse(Console.ReadLine(), out result) && result != 0)
+        if (int.TryParse(Console.ReadLine(), out result) && result >= 10000 && result < 100000)
         {
             break;
         } else {
-            Console.WriteLine("Вы ввели Неверное значение. введите число, кроме нуля");
+            Console.WriteLine("Вы ввели Неверное значение. введите пятизначное число");
         }
 
     }
     return result;
 }
 
-int cube(int number){
-    return number * number * number;
+string palindrome(int number){
+    if(number / 10000 == number % 10 && (number / 1000) % 10 == number % 100 / 10)
+    return "да";
+    else {
+        return "нет";
+    }
 }
 
 int number = GetMyNumber();
-int count = 1;
-Console.Write($"{number} -> ");
-while(count < number){
-    Console.Write(cube(count) + ", ");
-    count++;
-}
-Console.Write(cube(number) + ".");
+Console.WriteLine($"{number} -> {palindrome(number)}");
